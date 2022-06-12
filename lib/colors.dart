192c1ad7c468c49e0ba48eb3ab8090014c5a6f75
hari1412 <harimoradiya123@gmail.com>
2022-06-12 14:49:03 +0530

    Final Commit

---
+++ lib/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Colours {
+  static const Color app_main = Color(0xFF019BE3);
+  static const Color line = Color(0x70FFFFFF);
+  static const Color circleBorder = Color(0xFFCDCDCD);
+  static const Color pressed = Color(0x20000000);
+  static const Color solid_line = Color(0x30FFFFFF);
+  static const Color dash_line = Color(0x20FFFFFF);
+  static const Color content = Color(0x0E000000);
+}
+
